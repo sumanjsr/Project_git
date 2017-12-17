e58746372c83386fdfe220222ca7672f50ed8177
sumanjsr <34605340+sumanjsr@users.noreply.github.com>
2017-12-17 09:38:46 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, 2nd Commit");
+      System.out.println("Hello, Poll SCM");
    }
 }
